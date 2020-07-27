03591c61eae8bd937810ed2af0a6510c0f3dcc10
felipebma <fbma@cin.ufpe.br>
2020-07-27 00:25:58 -0300

    Implementing possible moves of Rook

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -18,6 +19,21 @@ public class Rook extends ChessPiece{
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		
+		Position p = new Position(0,0);
+		int[][] directions = {{-1,0},{1,0},{0,-1},{0,1}};
+		for(int[] direction:directions) {
+			p.setValues(position.getRow() + direction[0], position.getColumn() + direction[1]);
+			while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+				p.setRow(p.getRow() + direction[0]);
+				p.setColumn(p.getColumn() + direction[1]);
+			}
+			if(getBoard().positionExists(p) && this.isThereOpponentPiece(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+			}
+		}		
+		
 		return mat;
 	}
 }
